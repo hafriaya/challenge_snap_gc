32596b3628e2912f5d73c3640fc65aa2a7d819bd
Aya Hafri <115592428+hafriaya@users.noreply.github.com>
2024-03-13 15:49:17 +0000

    hamberger_nav_bar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,48 @@
 const humberger = document.querySelector(
   ".header__nav-bar__nav-menu__humberger"
 );
-const navMenu = document.querySelector(".header__nav-bar__nav-menu");
+const navBar = document.querySelector(".header__nav-bar");
 
 humberger.addEventListener("click", mobileMenu);
 
+// mobileMenu function
 function mobileMenu() {
+  console.log("mobile menu");
   humberger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  navBar.classList.toggle("active");
+}
+console.log("script running");
+
+const navLink = document.querySelectorAll(
+  ".header__nav-bar__nav-menu__nav-item__nav-link"
+);
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  humberger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
-const navLink = document.querySelectorAll(".header__nav-bar__nav-menu__nav-item__nav-link");
 
-  navLink.forEach((n) => n.addEventListener("click", closeMenu));
+// dropdown function
+navLink.forEach((dropdownTrigger) => {
+  dropdownTrigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const dropdownContent = dropdownTrigger.nextElementSibling;
+    dropdownContent.classList.toggle("show");
+  });
+});
 
-  function closeMenu() {
-    humberger.classList.remove("active");
-    navMenu.classList.remove("active");
+// arrow function
+function changeArrow() {
+  const arrow = document.querySelector(
+    ".header__nav-bar__nav-menu__nav-item__nav-link__arrow"
+  );
+  const arrowSrc = arrow.src;
+  if (arrowSrc.includes("arrow-down")) {
+    arrow.src = "./assets/images/icon-arrow-up.svg";
+  } else {
+    arrow.src = "./assets/images/icon-arrow-down.svg";
   }
+}
