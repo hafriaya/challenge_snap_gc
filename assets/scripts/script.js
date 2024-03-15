a6154cb7d69a62a43ca424b2fc440d38a5d7fdd2
Aya Hafri <115592428+hafriaya@users.noreply.github.com>
2024-03-15 14:15:22 +0000

    dropdown_script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,48 +1,58 @@
-const humberger = document.querySelector(
-  ".header__nav-bar__nav-menu__humberger"
+const hamberger = document.querySelector(
+  ".header__navigation__nav-bar__nav-menu__hamberger"
 );
-const navBar = document.querySelector(".header__nav-bar");
+const navBar = document.querySelector(".header__navigation__nav-bar");
 
-humberger.addEventListener("click", mobileMenu);
+const navLink = document.querySelectorAll(
+  ".header__navigation__nav-bar__nav-menu__nav-item__nav-link"
+);
 
+
+hamberger.addEventListener("click", mobileMenu);
 // mobileMenu function
 function mobileMenu() {
   console.log("mobile menu");
-  humberger.classList.toggle("active");
+  hamberger.classList.toggle("active");
   navBar.classList.toggle("active");
 }
-console.log("script running");
 
-const navLink = document.querySelectorAll(
-  ".header__nav-bar__nav-menu__nav-item__nav-link"
-);
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+navLink.forEach(n => n.addEventListener("click", () => {}));
 
 function closeMenu() {
-  humberger.classList.remove("active");
-  navMenu.classList.remove("active");
+  hamberger.classList.remove("active");
+  navBar.classList.remove("active");
 }
 
 
 // dropdown function
 navLink.forEach((dropdownTrigger) => {
   dropdownTrigger.addEventListener("click", function (event) {
-    event.preventDefault();
     const dropdownContent = dropdownTrigger.nextElementSibling;
     dropdownContent.classList.toggle("show");
+
+    const clickedArrow = dropdownTrigger.querySelector(
+      ".header__navigation__nav-bar__nav-menu__nav-item__nav-link__arrow"
+    );
+    if (clickedArrow) {
+      changeArrow(clickedArrow);
+    }
   });
 });
 
 // arrow function
-function changeArrow() {
-  const arrow = document.querySelector(
-    ".header__nav-bar__nav-menu__nav-item__nav-link__arrow"
-  );
-  const arrowSrc = arrow.src;
+function changeArrow(clickArrow) {
+  // const arrow = document.querySelector(
+  //   ".header__navigation__nav-bar__nav-menu__nav-item__nav-link__arrow"
+  // );
+  if(clickArrow){
+  const arrowSrc = clickArrow.src;
   if (arrowSrc.includes("arrow-down")) {
-    arrow.src = "./assets/images/icon-arrow-up.svg";
+    clickArrow.src = "./assets/images/icon-arrow-up.svg";
   } else {
-    arrow.src = "./assets/images/icon-arrow-down.svg";
-  }
+    clickArrow.src = "./assets/images/icon-arrow-down.svg";
+  }}
 }
+
+console.log("script running");
